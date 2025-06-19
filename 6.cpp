fcfdbad9e6cf77b1965772db9750ab6b2490402a
jesesok <jeses47@gmail.com>
2025-06-19 16:17:53 +0300

    6.1 6.2

--- 6.cpp
+++ 6.cpp
@@ -1,18 +1,45 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> privet (){
+std::vector<int> vectFill () {
 	std::vector<int> ish;
 	int value;
-	while (true){
-		if (!(std::cin >> value)){
+	while (true) {
+		std::cout << "Введите число ";
+		if (!(std::cin >> value)) {
 			break;
 		}
 		ish.push_back(value);
-		
 	}
+	return ish;
 }
 
-int main(){
-	return 0;
+// ex. 6.1
+// std::vector<int> vectChanger(std::vector<int> incomeVector) {
+// 	std::vector<int> newVector;
+// 	for (int i = 0; i < incomeVector.size(); i+=2) {
+// 		int a = (incomeVector[i] + incomeVector[i + 1]);
+// 		newVector.push_back(a);
+// 	}
+// 	return newVector;
+// }
+
+std::vector<int> vectChanger(std::vector<int> incomeVector) {
+	std::vector<int> newVector;
+	int vectSize = incomeVector.size();
+	for (int i = 0; i < incomeVector.size()/2; i ++) {
+		int a = (incomeVector[i] + incomeVector[vectSize - i - 1]);
+		newVector.push_back(a);
+	}
+	
+	return newVector;
+}
+
+int main() {
+	std::vector<int> a = vectFill();
+	std::vector<int> output = vectChanger(a);
+	for (int i = 0; i<output.size(); i++){
+		std::cout << output[i] << " ";
+	}
+		return 0;
 }
